980d864699066a3f5f05100cf164ab307abf0be2
mark-1701 <vini.muralles@gmail.com>
2024-01-22 19:41:07 -0600

    seventh commit

--- app/components/ListItems.js
+++ app/components/ListItems.js
@@ -5,16 +5,16 @@ export function ListItems() {
     items += `<li class="item list-group-item"><a href="#/article/${el}">${el}</a></li>`;
   });
 
-  document.addEventListener("mouseover", e => {
-    if (e.target.matches(".item") || e.target.matches(".item *")) {
-      if (e.target.tagName === "A") e.target.parentElement.classList.add("active");
-      e.target.classList.add("active");
-    }
-  });
-  document.addEventListener("mouseout", e => {
-    if (e.target.matches(".item") || e.target.matches(".item *")) {
-      e.target.classList.remove("active");
-    }
-  });
+  // document.addEventListener("mouseover", e => {
+  //   if (e.target.matches(".item") || e.target.matches(".item *")) {
+  //     if (e.target.tagName === "A") e.target.parentElement.classList.add("active");
+  //     e.target.classList.add("active");
+  //   }
+  // });
+  // document.addEventListener("mouseout", e => {
+  //   if (e.target.matches(".item") || e.target.matches(".item *")) {
+  //     e.target.classList.remove("active");
+  //   }
+  // });
   return items;
 }
